89a5ee2e84b484fb6bed8e161c04d7315bd120f1
binIaden <elrabos904@gmail.com>
2026-08-26 03:38:57 -0500

    Update global.py

--- global.py
+++ global.py
@@ -7,7 +7,7 @@ from telethon.sessions import StringSession
 # ============================================================
 # CONFIGURACIÓN (variables de entorno para Railway)
 # ============================================================
-API_ID = int(os.environ.get("API_ID")
+API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
 BOT = "@Globalccvs_Bot"
